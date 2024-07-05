531e7127da4a9a79cf031983998239f167073348
Bernardo <bernardosg@me.com>
2024-07-05 19:29:04 -0300

    first commit

---
+++ negg-counter.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         [GC] Neggs Counter
+// @namespace    https://greasyfork.org/users/1230396
+// @version      1.0
+// @description  Show how many Negg Tokens you have in the inventory
+// @author       Berna
+// @match        https://www.grundos.cafe/winter/neggery/
+// @icon         https://i.imgur.com/gDxnNrn.gif
+// @license      MIT
+ 
+// ==/UserScript==
+ 
+function findElementByText(text) {
+  var elements = document.querySelectorAll('p');
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].textContent.includes(text)) {
+      return elements[i];
+    }
+  }
+  return null;
+}
+ 
+var yourNeggs = findElementByText('Your Neggs');
+ 
+if (yourNeggs) {
+  var neggPoints = Array.from(document.getElementsByClassName('item-info')).reduce(function (total, itemInfo) {
+    var match = itemInfo.textContent.match(/\d+/);
+    return total + (match ? parseInt(match[0], 10) : 0);
+  }, 0);
+ 
+  var combinedHtml = yourNeggs.innerHTML + ' (</strong>' + neggPoints + ' Negg Tokens available in your inventory)';
+ 
+  yourNeggs.innerHTML = combinedHtml;
+}
