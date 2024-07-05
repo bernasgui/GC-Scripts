cb06ba8685c2d2060811dfc04fd7bb5cb09bc797
Bernardo <bernardosg@me.com>
2024-07-05 19:44:36 -0300

    github and greasyfork are both integrated

--- negg-counter.user.js
+++ negg-counter.user.js
@@ -2,13 +2,12 @@
 // @name         [GC] Neggs Counter
 // @namespace    https://greasyfork.org/users/1230396
 // @namespace    https://github.com/bernasgui/
-// @version      1.0
+// @version      1.1
 // @description  Show how many Negg Tokens you have in the inventory
 // @author       Berna
 // @match        https://www.grundos.cafe/winter/neggery/
 // @icon         https://i.imgur.com/gDxnNrn.gif
 // @license      MIT
- 
 // ==/UserScript==
  
 function findElementByText(text) {
